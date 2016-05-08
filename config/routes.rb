944428365160426489f1e2835e6f560c1c501cde
VictorWinberg <dat14vwi@student.lu.se>
2016-05-08 19:42:23 +0200

    rails g scaffold article

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -14,6 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :articles
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
